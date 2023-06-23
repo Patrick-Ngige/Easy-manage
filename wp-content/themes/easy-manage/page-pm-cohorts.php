93a56bacfa33d38c64e830b9e06fc4ab309978ef
Patrick-Ngige <wakemanjajr@gmail.com>
2023-06-23 16:11:09 +0300

    dynamic:trainers and trainees in pm dashboard

--- wp-content/themes/easy-manage/page-pm-cohorts.php
+++ wp-content/themes/easy-manage/page-pm-cohorts.php
@@ -1,9 +1,10 @@
-<?php get_header();
+<?php
+get_header();
 
 /**
- * Template Name: PM Cohorts
- * 
+ * Template Name: Admin PM List
  */
+
 $current_user = wp_get_current_user();
 
 ?>
@@ -11,124 +12,76 @@ $current_user = wp_get_current_user();
 <div style="width:100vw;height:90vh;display:flex;flex-direction:row;margin-top:-2.45rem">
 
     <div class="page-trainee-dashboard" style="margin-top:-1.99rem;width:20vw">
-        <?php get_template_part('sidenav-pm'); ?>
+        <?php get_template_part('sidenav-admin'); ?>
     </div>
+
 
     <div style="padding:1rem;width:80vw;margin-left:0rem">
         <div style="padding:1rem;">
-
-            <?php echo do_shortcode('[search_bar]'); ?>
+            <!-- Add buttons and search bar here -->
+            <div style="display: flex; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+                <a href="http://localhost/easy-manage/admin-trainers-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainers
+                </a>
+                <a href="http://localhost/easy-manage/admin-trainees-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainees
+                </a>
+                <?php echo do_shortcode('[search_bar]'); ?>
+            </div>
 
             <table class="table align-middle mb-0 bg-white table-hover"
-                style="width:80%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
+                style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
                 <thead class="bg-light">
-                    <tr style="font-size:large;color:#315B87;padding-left:1rem">
+                    <tr style="font-size:large;color:#315B87;padding-left:2rem">
                         <th>Cohort</th>
                         <th>Trainer</th>
-                        <th>Ending Date</th>
-                        <th></th>
+                        <th>Ending date</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <div class="d-flex align-items-center">
-                                <div class="ms-3">
-                                    <p class="fw-normal mb-1">WordPress
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="fw-normal mb-1">Jon Doe</p>
-                        </td>
-                        <td>
-                            <p>30/06/2023</p>
-                        </td>
-                        <td>
-                        <form method="POST">
-                            <a href="http://localhost/easy-manage/single-page/" style="padding:6px"><img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/arrow-right.png"
-                                    style="width:35px;" alt=""></a> &nbsp;&nbsp;
-                         <input type="hidden" name="employee_id" value="">
-                        </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="d-flex align-items-center">
-                                <div class="ms-3">
-                                    <p class="fw-normal mb-1">WordPress
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="fw-normal mb-1">Jon Doe</p>
-                        </td>
-                        <td>
-                            <p>30/06/2023</p>
-                        </td>
-                        <td>
-                        <form method="POST">
-                            <a href="http://localhost/easy-manage/single-page/" style="padding:6px"><img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/arrow-right.png"
-                                    style="width:35px;" alt=""></a> &nbsp;&nbsp;
-                         <input type="hidden" name="employee_id" value="">
-                        </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="d-flex align-items-center">
-                                <div class="ms-3">
-                                    <p class="fw-normal mb-1">WordPress
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="fw-normal mb-1">Jon Doe</p>
-                        </td>
-                        <td>
-                            <p>30/06/2023</p>
-                        </td>
-                        <td>
-                        <form method="POST">
-                            <a href="http://localhost/easy-manage/single-page/" style="padding:6px"><img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/arrow-right.png"
-                                    style="width:35px;" alt=""></a> &nbsp;&nbsp;
-                         <input type="hidden" name="employee_id" value="">
-                        </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="d-flex align-items-center">
-                                <div class="ms-3">
-                                    <p class="fw-normal mb-1">WordPress
-                                    </p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="fw-normal mb-1">Jon Doe</p>
-                        </td>
-                        <td>
-                            <p>30/06/2023</p>
-                        </td>
-                        <td>
-                        <form method="POST">
-                            <a href="http://localhost/easy-manage/single-page/" style="padding:6px"><img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/arrow-right.png"
-                                    style="width:35px;" alt=""></a> &nbsp;&nbsp;
-                         <input type="hidden" name="employee_id" value="">
-                        </form>
-                        </td>
-                    </tr>
+                    <?php
+                    $request_url = 'http://localhost/easy-manage/wp-json/em/v1/projects/cohorts';
+                    $response = wp_remote_get($request_url);
+                    $cohort = wp_remote_retrieve_body($response);
+                    $cohort = json_decode($cohort, true);
+
+                    if (is_array($cohort)) {
+                        foreach ($cohort as $Cohort) {
+
+                            echo '<tr>';
+                            echo '<td>';
+                            echo '<div class="d-flex align-items-center">';
+                            echo '<div class="ms-3">';
+                            echo '<p class="mb-1">' . $Cohort['cohort'] . '</p>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1">' . $Cohort['trainer'] . '</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1">' .$Cohort['ending_date'] .'</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<form method="POST">';
+                            echo '<a href="http://localhost/easy-manage/admin-update-form/?id=' . $Cohort['cohort_id'] . '" style="padding:6px"><img src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png" style="width:25px;" alt=""></a> &nbsp;&nbsp;';
+                            echo '<input type="hidden" name="" value="">';
+                            echo '<a href="#" style="padding:6px;text-decoration:none;color:#315B87"> <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/pause-2.png" style="width:25px;" alt="">  </a> &nbsp;&nbsp;';
+                            echo '</form>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo 'Error retrieving cohort';
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
     </div>
+</div>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
--- wp-content/themes/easy-manage/page-pm-trainers.php
+++ wp-content/themes/easy-manage/page-pm-trainers.php
@@ -1,9 +1,10 @@
-<?php get_header();
+<?php
+get_header();
 
 /**
- * Template Name: PM Trainers
- * 
+ * Template Name: Admin PM List
  */
+
 $current_user = wp_get_current_user();
 
 ?>
@@ -14,89 +15,73 @@ $current_user = wp_get_current_user();
         <?php get_template_part('sidenav-pm'); ?>
     </div>
 
+
     <div style="padding:1rem;width:80vw;margin-left:0rem">
         <div style="padding:1rem;">
-          
-            <?php echo do_shortcode('[search_bar]'); ?>
+            <!-- Add buttons and search bar here -->
+            <div style="display: flex; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+                <a href="http://localhost/easy-manage/admin-trainers-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainers
+                </a>
+                <a href="http://localhost/easy-manage/admin-trainees-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainees
+                </a>
+                <?php echo do_shortcode('[search_bar]'); ?>
+            </div>
 
-    <table class="table align-middle  bg-white table-hover"
-        style="width:80%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;height:fit-content">
-        <thead class="bg-light">
-            <tr style="font-size:large;color:#315B87;padding-left:2rem;">
-                <th class="ms-3">Trainer</th>
-                <th>Email</th>
-                <th>Role</th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                        <div class="ms-3">
-                            <p class="fw-normal mb-1">Jon Doe
-                            </p>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">jon@yopmail.com</p>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Trainer</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                        <div class="ms-3">
-                            <p class="fw-normal mb-1">Jon Doe
-                            </p>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">jon@yopmail.com</p>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Trainer</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                        <div class="ms-3">
-                            <p class="fw-normal mb-1">Jon Doe
-                            </p>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">jon@yopmail.com</p>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Trainer</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                        <div class="ms-3">
-                            <p class="fw-normal mb-1">Jon Doe
-                            </p>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">jon@yopmail.com</p>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Trainer</p>
-                </td>
-            </tr>
+            <table class="table align-middle mb-0 bg-white table-hover"
+                style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
+                <thead class="bg-light">
+                    <tr style="font-size:large;color:#315B87;padding-left:2rem">
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $request_url = 'http://localhost/easy-manage/wp-json/em/v1/users/trainers';
+                    $response = wp_remote_get($request_url);
+                    $users = wp_remote_retrieve_body($response);
+                    $users = json_decode($users, true);
 
-        </tbody>
-    </table>
-</div>
+                    if (is_array($users)) {
+                        foreach ($users as $user) {
+
+                            echo '<tr>';
+                            echo '<td>';
+                            echo '<div class="d-flex align-items-center">';
+                            echo '<div class="ms-3">';
+                            echo '<p class="mb-1">' . $user['user_nicename'] . '</p>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1">' . $user['user_email'] . '</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1"> Trainer</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<form method="POST">';
+                            echo '<a href="http://localhost/easy-manage/admin-update-form/?id=' . $user['ID'] . '" style="padding:6px"><img src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png" style="width:25px;" alt=""></a> &nbsp;&nbsp;';
+                            echo '<input type="hidden" name="" value="">';
+                            echo '<a href="#" style="padding:6px;text-decoration:none;color:#315B87"> <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/pause-2.png" style="width:25px;" alt="">  </a> &nbsp;&nbsp;';
+                            echo '</form>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo 'Error retrieving users';
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
