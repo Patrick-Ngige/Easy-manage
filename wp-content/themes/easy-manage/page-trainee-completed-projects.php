59a8084a0982ff6d8f9c02278317a64e5369b4b1
Patrick-Ngige <wakemanjajr@gmail.com>
2023-06-24 13:18:28 +0300

    addressing completed projects

--- wp-content/themes/easy-manage/page-trainee-completed-projects.php
+++ wp-content/themes/easy-manage/page-trainee-completed-projects.php
@@ -1,159 +1,91 @@
-<?php get_header();
+<?php
+get_header();
 
 /**
- * Template Name: Completed Projects
- * 
+ * Template Name: Admin PM List
  */
+
+$current_user = wp_get_current_user();
 
 ?>
 
-<div style="width:100vw;height:90vh;display:flex;flex-direction:row;margin-top:-4.45rem">
+<div style="width:100vw;height:90vh;display:flex;flex-direction:row;margin-top:-2.45rem">
 
-    <div class="page-trainee-dashboard" style="width:20vw">
+    <div class="page-trainee-dashboard" style="margin-top:-1.99rem;width:20vw">
         <?php get_template_part('sidenav-trainee'); ?>
     </div>
 
-    <div style="padding:1rem;width:80vw;margin-top:3rem;">
-    <?php echo do_shortcode('[search_bar]'); ?>
-    <table class="table align-middle mb-0 bg-white table-hover"
-            style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:6%;">
-            <thead class="bg-light">
-                <tr style="font-size:large;color:#315B87;padding-left:2rem">
-                    <th>Project</th>
-                    <th>Status</th>
-                    <th>Due Date</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <div class="d-flex align-items-center">
-                            <div class="ms-3">
-                                <p class="mb-1">Easy-manage website
-                                </p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                    <p class="fw-normal mb-1">Completed</p>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">29/06/2023</p>
-                    </td>
-                    <td>
-                        <form method="POST">
-                            <input type="hidden" name="employee_id" value="">
-                            <button type="submit" name="delete" value=""
-                                style="padding:5px;border:none;background-color:#FAFAFA" onclick=""> <img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/delete.png"
-                                    style="width:25px" alt=""></button>
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                <td>
-                        <div class="d-flex align-items-center">
-                            <div class="ms-3">
-                                <p class="mb-1">Easy-manage website
-                                </p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                    <p class="fw-normal mb-1">Completed</p>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">29/06/2023</p>
-                    </td>
-                    <td>
-                    <form method="POST">
 
-                            <input type="hidden" name="employee_id" value="">
-                            <button type="submit" name="delete" value=""
-                                style="padding:5px;border:none;background-color:#FAFAFA" onclick=""> <img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/delete.png"
-                                    style="width:25px" alt=""></button>
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                <td>
-                        <div class="d-flex align-items-center">
-                            <div class="ms-3">
-                                <p class="mb-1">Easy-manage website
-                                </p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                    <p class="fw-normal mb-1">Completed</p>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">29/06/2023</p>
-                    </td>
-                    <td>
-                    <form method="POST">
-                            <input type="hidden" name="employee_id" value="">
-                            <button type="submit" name="delete" value=""
-                                style="padding:5px;border:none;background-color:#FAFAFA" onclick=""> <img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/delete.png"
-                                    style="width:25px" alt=""></button>
-                        </form>
-                    </td>
-                </tr>
+    <div style="padding:1rem;width:80vw;margin-left:0rem">
+        <div style="padding:1rem;">
+            <!-- Add buttons and search bar here -->
+            <div style="display: flex; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+                <a href="http://localhost/easy-manage/admin-trainers-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainers
+                </a>
+                <a href="http://localhost/easy-manage/admin-trainees-table/" class="floating-btn"
+                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    View Trainees
+                </a>
+                <?php echo do_shortcode('[search_bar]'); ?>
+            </div>
 
-                <tr>
-                <td>
-                        <div class="d-flex align-items-center">
-                            <div class="ms-3">
-                                <p class="mb-1">Easy-manage website
-                                </p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                    <p class="fw-normal mb-1">Completed</p>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">29/06/2023</p>
-                    </td>
-                    <td>
-                    <form method="POST">
-                            <input type="hidden" name="employee_id" value="">
-                            <button type="submit" name="delete" value=""
-                                style="padding:5px;border:none;background-color:#FAFAFA" onclick=""> <img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/delete.png"
-                                    style="width:25px" alt=""></button>
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                <td>
-                        <div class="d-flex align-items-center">
-                            <div class="ms-3">
-                                <p class="mb-1">Easy-manage website
-                                </p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">Completed</p>
-                    </td>
-                    <td>
-                        <p class="fw-normal mb-1">29/06/2023</p>
-                    </td>
-                    <td>
-                    <form method="POST">
-                            <input type="hidden" name="employee_id" value="">
-                            <button type="submit" name="delete" value=""
-                                style="padding:5px;border:none;background-color:#FAFAFA" onclick=""> <img
-                                    src="http://localhost/easy-manage/wp-content/uploads/2023/06/delete.png"
-                                    style="width:25px" alt=""></button>
-                        </form>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            <table class="table align-middle mb-0 bg-white table-hover"
+                style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
+                <thead class="bg-light">
+                    <tr style="font-size:large;color:#315B87;padding-left:2rem">
+                        <th>Project</th>
+                        <th>Type</th>
+                        <th>Due Date</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $request_url = 'http://localhost/easy-manage/wp-json/em/v1/projects/completed';
+                    $response = wp_remote_get($request_url);
+                    $projects = wp_remote_retrieve_body($response);
+                    $projects = json_decode($projects, true);
+
+                    if (is_array($projects)) {
+                        foreach ($projects as $completed) {
+
+                            echo '<tr>';
+                            echo '<td>';
+                            echo '<div class="d-flex align-items-center">';
+                            echo '<div class="ms-3">';
+                            echo '<p class="mb-1">' . $completed['project_name'] . '</p>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1">' . (isset($completed['assigned_members']) ? 'Group' : 'Individual') . '</p>';
+                            echo '</td>';                            
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1">' . $completed['due_date'] . '</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<p class="fw-normal mb-1" style="color:#146830"> Completed</p>';
+                            echo '</td>';
+                            echo '<td>';
+                            echo '<form method="POST">';
+                            echo '<a href="http://localhost/easy-manage/admin-update-form/?id=' . (isset($completed['group_id']) ? $completed['group_id'] : '') . (isset($completed['project_id']) ? '&id=' . $completed['project_id'] : '') . '" style="padding:6px"><img src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png" style="width:25px;" alt=""></a> &nbsp;&nbsp;';
+                            echo '<input type="hidden" name="" value="">';
+                            echo '<a href="#" style="padding:6px;text-decoration:none;color:#315B87"> <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/pause-2.png" style="width:25px;" alt="">  </a> &nbsp;&nbsp;';
+                            echo '</form>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo 'Error retrieving projects';
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
+
+<?php get_footer(); ?>
