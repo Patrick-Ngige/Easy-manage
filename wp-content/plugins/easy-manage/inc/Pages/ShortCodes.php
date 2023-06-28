d40b8339331e13c9c142e65f0082e30c9872dda6
Patrick-Ngige <wakemanjajr@gmail.com>
2023-06-28 20:51:09 +0300

    working on search form

--- wp-content/plugins/easy-manage/inc/Pages/ShortCodes.php
+++ wp-content/plugins/easy-manage/inc/Pages/ShortCodes.php
@@ -41,20 +41,54 @@ class ShortCodes
             }
         </style>
 
-        <form role="search" method="get" action="http://localhost/easy-manage/search-results/">
+        <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
             <label>
                 <span class="screen-reader-text">
-                    <?php echo _x('Search for:', 'label', 'your-theme-domain'); ?>
+                    <?php echo esc_html_x('Search for:', 'label', 'your-theme-domain'); ?>
                 </span>
                 <input type="search" class="search-field"
                     placeholder="<?php echo esc_attr_x('Search...', 'placeholder', 'your-theme-domain'); ?>"
                     value="<?php echo get_search_query(); ?>" name="s" />
 
             </label>
-            <button type="submit" class="search-submit">
-                <?php echo esc_html_x('Search', 'submit button', '='); ?>
+         <?php   $searchQuery = get_search_query(); ?>
+            <input type="hidden" name="query" value="<?php echo esc_attr($searchQuery); ?>">
+            <button type="submit" class="search-submit" name="searchbtn">
+                <?php echo esc_html_x('Search', 'submit button', 'your-theme-domain'); ?>
             </button>
         </form>
+
+        <div class="search-results">
+
+            <?php
+
+           
+
+            if (isset($_POST['searchbtn'])) {
+                $searchQuery = $_POST['query'];
+            }
+
+            if ($searchQuery) {
+                $searchEndpoint = 'http://localhost/easy-manage/wp-json/em/v1/users/search?=&name=' . urlencode($searchQuery);
+
+                $response = wp_remote_get($searchEndpoint);
+                if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) {
+                    $results = json_decode(wp_remote_retrieve_body($response), true);
+                    if ($results) {
+                        foreach ($results as $result) {
+                            echo '<div class="search-result">';
+                            echo '<h3>' . esc_html($result['name']) . '</h3>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo '<p>No results found.</p>';
+                    }
+                } else {
+                    echo '<p>Error occurred while performing the search.</p>';
+                }
+            }
+            ?>
+        </div>
 
 
         <?php
