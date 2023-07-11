3ae902db6fccf778b3916b2bbe8204db6d667e08
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-12 01:12:26 +0300

    deactivated projects table is scrollable with transparent scrollbars

--- wp-content/themes/easy-manage/page-trainer-deactivated-projects.php
+++ wp-content/themes/easy-manage/page-trainer-deactivated-projects.php
@@ -7,7 +7,6 @@ get_header();
 
 $token = $_COOKIE['token'];
 
-
 $request_url = 'http://localhost/easy-manage/wp-json/em/v1/pm';
 $response = wp_remote_get(
     $request_url,
@@ -26,7 +25,6 @@ $endpoint = '';
 if (isset($_POST['restore_user'])) {
     $user_id = $_POST['user_id'];
     $endpoint = 'http://localhost/easy-manage/wp-json/em/v1/restore_user/' . $user_id;
-
 }
 
 $ch = curl_init($endpoint);
@@ -45,7 +43,6 @@ $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 if ($http_status === 200) {
     $response = json_decode($result, true);
-
 } else {
 
 }
@@ -62,75 +59,74 @@ curl_close($ch);
         <div style="padding:1rem;">
             <!-- Add buttons and search bar here -->
             <div style="display: flex; align-items: center; justify-content: end; margin-bottom: 1rem;">
-              
                 <?php echo do_shortcode('[search_bar]'); ?>
             </div>
+            <div class="scrollable-container" style="height: 400px; overflow: auto;">
+                <table class="table align-middle mb-0 bg-white table-hover"
+                    style="width: 90%; margin-left: 5%; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; margin-top: 3%;">
+                    <thead class="bg-light fixed-thead">
+                        <tr style="font-size: large; color: #315B87; padding-left: 2rem;">
+                            <th>Trainee</th>
+                            <th>Project</th>
+                            <th>Status</th>
+                            <th>Restore</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $request_url = 'http://localhost/easy-manage/wp-json/em/v1/projects/deleted';
+                        $response = wp_remote_get($request_url);
+                        $projects = wp_remote_retrieve_body($response);
+                        $projects = json_decode($projects, true);
 
-            <table class="table align-middle mb-0 bg-white table-hover"
-                style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
-                <thead class="bg-light">
-                    <tr style="font-size:large;color:#315B87;padding-left:2rem">
-                        <th>Trainee</th>
-                        <th>Project</th>
-                        <th>Status</th>
-                        <th>Restore</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $request_url = 'http://localhost/easy-manage/wp-json/em/v1/projects/deleted';
-                    $response = wp_remote_get($request_url);
-                    $projects = wp_remote_retrieve_body($response);
-                    $projects = json_decode($projects, true);
+                        if (is_array($projects)) {
+                            foreach ($projects as $project) { ?>
 
-                    if (is_array($projects)) {
-                        foreach ($projects as $project) { ?>
-
-                            <tr>
-                                <td>
-                                    <div class="d-flex align-items-center">
-                                        <div class="ms-3">
-                                        <p class="mb-1">
-                                                <?php
-                                                if ($project['project_type'] === 'individual') {
-                                                    echo $project['assignee'];
-                                                } elseif ($project['project_type'] === 'group') {
-                                                    echo is_array($project['assigned_members']) ? implode(', ', $project['assigned_members']) : $project['assigned_members'];
-                                                }
-                                                ?>
-                                            </p>
+                                <tr>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <div class="ms-1">
+                                                <p class="mb-1">
+                                                    <?php
+                                                    if ($project['project_type'] === 'individual') {
+                                                        echo $project['assignee'];
+                                                    } elseif ($project['project_type'] === 'group') {
+                                                        echo is_array($project['assigned_members']) ? implode(', ', $project['assigned_members']) : $project['assigned_members'];
+                                                    }
+                                                    ?>
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p class="fw-normal mb-1">
-                                        <?php echo $project['project_task'] ?>
-                                    </p>
-                                </td>
-                                <td>
-                                    <p class="fw-normal mb-1" style="color:red">
-                                        Deleted
-                                    </p>
-                                </td>
-                                <td>
-                                    <form method="POST">
-                                        <input type="hidden" name="user_id" value="<?php echo $user['ID']; ?>">
-                                        <button type="submit" name="soft_delete" class="btn-soft-delete"
-                                            style="padding:6px;border:none">
-                                            <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/reuse.png"
-                                                style="width:3vw;" alt="">
-                                        </button>
-
-                                    </form>
-                                </td>
-                            </tr>
-                        <?php }
-                    } else {
-                        echo '<tr><td colspan="4" style="text-align: center;">No deactivated trainers available</td></tr>';
-                    }
-                    ?>
-                </tbody>
-            </table>
+                                    </td>
+                                    <td>
+                                        <p class="fw-normal mb-1">
+                                            <?php echo $project['project_task'] ?>
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="fw-normal mb-1" style="color: red;">
+                                            Deleted
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <form method="POST">
+                                            <input type="hidden" name="user_id" value="<?php echo $user['ID']; ?>">
+                                            <button type="submit" name="soft_delete" class="btn-soft-delete"
+                                                style="padding: 6px; border: none;">
+                                                <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/reuse.png"
+                                                    style="width: 3vw;" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php }
+                        } else {
+                            echo '<tr><td colspan="4" style="text-align: center;">No deactivated trainers available</td></tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
@@ -138,3 +134,28 @@ curl_close($ch);
 
 get_footer();
 ?>
+<style>
+   .fixed-thead {
+        position: sticky;
+        top: 0;
+        background-color: #fff;
+        z-index: 1;
+    }
+    
+    .scrollable-container {
+        height: 400px;
+        overflow: auto;
+    }
+
+    .scrollable-container::-webkit-scrollbar {
+        width: 0.5rem;
+    }
+
+    .scrollable-container::-webkit-scrollbar-thumb {
+        background-color: transparent;
+    }
+
+    .scrollable-container::-webkit-scrollbar-track {
+        background-color: transparent;
+    }
+</style>
