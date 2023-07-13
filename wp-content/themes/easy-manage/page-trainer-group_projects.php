f54c05d9a614252a8a3e4946e41baf5583868ea6
Patrick-Ngige <wakemanjajr@gmail.com>
2023-07-13 14:01:10 +0300

    introduced button toggles for individual projects table

--- wp-content/themes/easy-manage/page-trainer-group_projects.php
+++ wp-content/themes/easy-manage/page-trainer-group_projects.php
@@ -114,13 +114,14 @@ if (isset($_POST['soft_delete'])) {
                             <td>
                                 <form method="POST">
                                 <input type="hidden" name="group_id" value="<?php echo $project['group_id']; ?>">
-                                        <button type="submit" name="soft_delete" class="btn-soft-delete"
+                                        <button type="submit" name="soft_delete" class="btn-soft-delete" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"
                                             style="padding:6px;border:none">
                                             <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/pause-2.png"
                                                 style="width:25px;" alt="">
                                         </button>
                                     <a href="http://localhost/easy-manage/update-group-project/?id=<?php echo $project['group_id'] ?>"
-                                        style="padding:6px"><img
+                                        style="padding:6px" data-bs-toggle="tooltip" data-bs-placement="top"
+                                            title="Edit"><img
                                             src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png"
                                             style="width:25px;" alt=""></a> &nbsp;&nbsp;
                                     
@@ -139,3 +140,8 @@ if (isset($_POST['soft_delete'])) {
 </div>
 
 <?php get_footer(); ?>
+<script>
+    $(function () {
+        $('[data-bs-toggle="tooltip"]').tooltip();
+    });
+</script>
