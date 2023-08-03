b613647633bed7ecf4d2964eee7e91f2b21f2bbe
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-03 22:09:28 +0300

    restructuring pm table

--- wp-content/themes/easy-manage/page-admin-trainees.php
+++ wp-content/themes/easy-manage/page-admin-trainees.php
@@ -51,10 +51,9 @@ if (isset($_POST['soft_delete'])) {
                 <?php echo do_shortcode('[search_bar]'); ?>
             </div>
 
-            <table class="table align-middle mb-0 bg-white table-hover"
-                style="width:90%;margin-left:5%;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;margin-top:3%;">
+            <table class="table align-middle table-hover">
                 <thead class="bg-light">
-                    <tr style="font-size:large;color:#315B87;padding-left:2rem">
+                    <tr class="table-tr">
                         <th>Username</th>
                         <th>Email</th>
                         <th>Role</th>
