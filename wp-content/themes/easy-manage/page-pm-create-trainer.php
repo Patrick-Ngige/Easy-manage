bf58224c24848dedc17c1c14825a55e0ee325d90
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-07 22:57:51 +0300

    removing inline css - create trainer

--- wp-content/themes/easy-manage/page-pm-create-trainer.php
+++ wp-content/themes/easy-manage/page-pm-create-trainer.php
@@ -72,7 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <div class="main-div">
 
-    <div class="page-trainee-dashboard" style="margin-top: -1.99rem; width: 20vw;">
+    <div class="page-pm-sidenav" >
         <?php get_template_part('sidenav-pm'); ?>
     </div>
     <div style="height: 80vh; margin-left: 15rem;">
@@ -158,6 +158,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <style>
     .main-div{
         width: 100vw; height: 90vh; display: flex; flex-direction: row; margin-top: -2.45rem;
+    }
+    .page-pm-sidenav{
+        margin-top: -1.99rem; width: 20vw;
     }
 </style>
 
