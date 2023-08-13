caed9b7b7d5078a51db3f223044eea6a98bcb5c0
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-13 14:54:13 +0300

    removing inline css - trainer update form

--- wp-content/themes/easy-manage/page-pm-trainer-update.php
+++ wp-content/themes/easy-manage/page-pm-trainer-update.php
@@ -134,7 +134,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                                 } ?>
                                             </div>
                                             <div
-                                                class="pt-1 mt-3 w-100 d-flex justify-content-center align-items-center">
+                                                class="button">
                                                 <button class="btn btn-lg btn-block w-50"
                                                     style="background-color: #315B87; color: #FAFAFA;margin-bottom:-2rem" type="submit"
                                                     name="updatebtn">Update</button>
@@ -194,7 +194,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     .form-label{
         font-weight: 600;
     }
-    
+    .button{
+        padding-top:.8rem; margin-top:1rem; display:flex; justify-content:center; align-items:center
+    }
 </style>
 
 <?php get_footer(); ?>
