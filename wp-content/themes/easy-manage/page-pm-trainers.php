8012413ccbf867406f79443f517fa2ccb2235064
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-13 15:23:24 +0300

    removing inline css - trainers table

--- wp-content/themes/easy-manage/page-pm-trainers.php
+++ wp-content/themes/easy-manage/page-pm-trainers.php
@@ -36,14 +36,14 @@ if (isset($_POST['soft_delete'])) {
 }
 ?>
 <div class="main-div">
-    <div class="page-trainee-dashboard" style="margin-top:-1.99rem;width:20vw">
+    <div class="page-pm-sidenav">
         <?php get_template_part('sidenav-pm'); ?>
     </div>
-    <div style="padding:1rem;width:80vw;margin-left:0rem">
-        <div style="padding:1rem;">
-            <div style="display: flex; align-items: center; justify-content:space-between; margin-bottom: 1rem;">
+    <div class="div-1">
+        <div class="div-2">
+            <div class="div-3">
             <a href="http://localhost/easy-manage/deleted-trainers/" class="floating-btn"
-                    style="text-decoration:none; padding: 0.5rem 1rem; border-radius: 10px; background-color: #FAFAFA; border: none; color: #315B87; font-size: 1rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                    >
                     Deactivated Trainers
                 </a>
                 <?php echo do_shortcode('[search_bar]'); ?>
@@ -121,8 +121,99 @@ if (isset($_POST['soft_delete'])) {
     </div>
 </div>
 <style>
-    .main-div{
-        width:100vw;height:90vh;display:flex;flex-direction:row;margin-top:-2.45rem
+    .main-div {
+        width: 100vw;
+        height: 90vh;
+        display: flex;
+        flex-direction: row;
+        margin-top: -2.45rem;
+    }
+
+    .page-pm-sidenav {
+        margin-top: -1.99rem;
+        width: 20vw;
+    }
+
+    .div-1 {
+        padding: 1rem;
+        width: 80vw;
+        margin-left: 0rem
+    }
+
+    .div-2 {
+        margin-top: 1rem;
+        padding: 1rem;
+    }
+
+    .div-3 {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 1rem;
+        padding-left: 1rem;
+    }
+
+    .floating-btn {
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        border-radius: 10px;
+        background-color: #FAFAFA;
+        border: none;
+        color: #315B87;
+        font-size: 1rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    .floating-btn:hover{
+        background-color: #315B87;
+        color: #FAFAFA;
+    }
+
+    .table {
+        width: 90%;
+        margin-left: 5%;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        margin-top: 3%;
+        background-color: #FAFAFA;
+        margin-bottom: 0;
+    }
+
+    .table-tr {
+        font-size: large;
+        color: #315B87;
+        padding-left: 2rem
+    }
+
+    .td-div-1 {
+        display: flex;
+        align-items: center;
+    }
+
+    .td-div-2 {
+        margin-left: 1rem;
+    }
+
+    .td-div-2 p {
+        margin-bottom: 1rem;
+    }
+
+    .td-2,
+    .td-3 {
+        font-weight: normal;
+        margin-bottom: 1rem;
+    }
+
+    .btn-soft-delete {
+        padding: 6px;
+        border: none
+    }
+
+    .btn-soft-delete img {
+        width: 25px;
+        border: none
+    }
+
+    .edit-btn img {
+        width: 25px;
     }
 </style>
 <?php get_footer(); ?>
