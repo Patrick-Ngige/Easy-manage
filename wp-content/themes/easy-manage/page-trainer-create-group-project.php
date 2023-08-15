efd4c1e673bf6205a2be33667f845e11544f50fd
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-15 12:12:24 +0300

    trainer-cohort: removing inline css

--- wp-content/themes/easy-manage/page-trainer-create-group-project.php
+++ wp-content/themes/easy-manage/page-trainer-create-group-project.php
@@ -104,8 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="div-2">
                         <div class="card">
                             <div class="div-3">
-                                <div class="row g-0 w-100 d-flex justify-content-center align-items-center w-50 "
-                                    style="width:40vw;">
+                                <div class="div-4">
                                     <div class="col-md-6 col-lg-7 d-flex justify-content-center align-items-center  ms-8"
                                         style="height:fit-content; width:40vw; ">
                                         <div class="card-body p-4 p-lg-5 text-black">
@@ -253,7 +252,7 @@ function selectMembers() {
     .card{
         border-radius: 1rem; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     }
-    .div-3{
+    .div-3, .div-4{
         flex-direction: row; gap:0; display:flex; justify-content:center; align-items:center;width:40vw;
     }
 </style>
