5ef64a885e19827b8bf303b7f1e6a6461c4c3326
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-16 13:31:30 +0300

    create-trainee: removing inline css

--- wp-content/themes/easy-manage/page-trainer-create-trainee.php
+++ wp-content/themes/easy-manage/page-trainer-create-trainee.php
@@ -39,7 +39,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Authorization: Bearer ' . $token
-        ));
+        )
+        );
         $response = curl_exec($curl);
         $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
@@ -64,130 +65,131 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php get_template_part('sidenav-trainer'); ?>
     </div>
     <div class="div-1">
-        <div class="container">
-            <div class="div-2">
-                <div class="div-3">
-                    <div class="card">
-                        <div class="div-4">
-                            <div class="div-4">
-                                <div class="div-5">
-                                    <div class="card-body">
-                                        <form action="" method="POST">
-                                            <h2>
-                                                Create trainee
-                                            </h2>
-                                            <?php if (isset($_GET['success']) && $_GET['success'] === 'true'): ?>
-                                                <div class="alert alert-success" role="alert">
-                                                    Trainer created successfully.
-                                                </div>
-                                            <?php endif; ?>
-                                            <div class="form-outline ">
-                                                <label class="form-label" for="form2Example27"
-                                                    >Trainee:</label>
-                                                <input type="text" id="form2Example27"
-                                                    class="form-control form-control-md"
-                                                    placeholder="Enter trainee name" name="trainee_name"
-                                                    value="<?php echo isset($_GET['trainee_name']) ? $_GET['trainee_name'] : ''; ?>" />
-                                                <?php if (isset($errors) && in_array('Trainee name is required', $errors)) {
-                                                    echo '<p class="text-danger">Trainee name is required</p>';
-                                                } ?>
-                                            </div>
-                                            <div class="form-outline ">
-                                                <label class="form-label" for="form2Example27"
-                                                    >Email:</label>
-                                                <input type="email" id="form2Example27"
-                                                    class="form-control form-control-md"
-                                                    placeholder="Enter trainee email" name="trainee_email"
-                                                    value="<?php echo isset($_GET['trainee_email']) ? $_GET['trainee_email'] : ''; ?>" />
-                                                <?php if (isset($errors) && in_array('Email is required', $errors)) {
-                                                    echo '<p class="text-danger">Email is required</p>';
-                                                } ?>
-                                            </div>
-                                            <div class="form-outline ">
-                                                <label class="form-label" for="form2Example27"
-                                                    >Role:</label>
-                                                <input type="text" id="form2Example27"
-                                                    class="form-control form-control-md"
-                                                    placeholder="Enter trainee role" name="trainee_role"
-                                                    value="<?php echo isset($_GET['trainee_role']) ? $_GET['trainee_role'] : ''; ?>" />
-                                                <?php if (isset($errors) && in_array('Role is required', $errors)) {
-                                                    echo '<p class="text-danger">Role is required</p>';
-                                                } ?>
-                                            </div>
-                                            <div class="form-outline">
-                                                <label class="form-label" for="form2Example27"
-                                                    >Password:</label>
-                                                <input type="password" id="form2Example27"
-                                                    class="form-control form-control-md"
-                                                    placeholder="Enter trainee role" name="trainee_password"
-                                                    value="<?php echo isset($_GET['trainee_password']) ? $_GET['trainee_password'] : ''; ?>" />
-                                                <?php if (isset($errors) && in_array('Password is required', $errors)) {
-                                                    echo '<p class="text-danger">Passsword is required</p>';
-                                                } ?>
-                                            </div>
-                                            <div
-                                                class="button">
-                                                <button class="create-btn"
-                                                   type="submit"
-                                                    name="createbtn">Create</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
+        <div class="div-3">
+            <div class="card">
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <h2>
+                            Create trainee
+                        </h2>
+                        <?php if (isset($_GET['success']) && $_GET['success'] === 'true'): ?>
+                            <div class="alert alert-success" role="alert">
+                                Trainer created successfully.
                             </div>
+                        <?php endif; ?>
+                        <div class="form-outline ">
+                            <label class="form-label" for="form2Example27">Trainee:</label>
+                            <input type="text" id="form2Example27" class="form-control form-control-md"
+                                placeholder="Enter trainee name" name="trainee_name"
+                                value="<?php echo isset($_GET['trainee_name']) ? $_GET['trainee_name'] : ''; ?>" />
+                            <?php if (isset($errors) && in_array('Trainee name is required', $errors)) {
+                                echo '<p class="text-danger">Trainee name is required</p>';
+                            } ?>
                         </div>
-                    </div>
+                        <div class="form-outline ">
+                            <label class="form-label" for="form2Example27">Email:</label>
+                            <input type="email" id="form2Example27" class="form-control form-control-md"
+                                placeholder="Enter trainee email" name="trainee_email"
+                                value="<?php echo isset($_GET['trainee_email']) ? $_GET['trainee_email'] : ''; ?>" />
+                            <?php if (isset($errors) && in_array('Email is required', $errors)) {
+                                echo '<p class="text-danger">Email is required</p>';
+                            } ?>
+                        </div>
+                        <div class="form-outline ">
+                            <label class="form-label" for="form2Example27">Role:</label>
+                            <input type="text" id="form2Example27" class="form-control form-control-md"
+                                placeholder="Enter trainee role" name="trainee_role"
+                                value="<?php echo isset($_GET['trainee_role']) ? $_GET['trainee_role'] : ''; ?>" />
+                            <?php if (isset($errors) && in_array('Role is required', $errors)) {
+                                echo '<p class="text-danger">Role is required</p>';
+                            } ?>
+                        </div>
+                        <div class="form-outline">
+                            <label class="form-label" for="form2Example27">Password:</label>
+                            <input type="password" id="form2Example27" class="form-control form-control-md"
+                                placeholder="Enter trainee role" name="trainee_password"
+                                value="<?php echo isset($_GET['trainee_password']) ? $_GET['trainee_password'] : ''; ?>" />
+                            <?php if (isset($errors) && in_array('Password is required', $errors)) {
+                                echo '<p class="text-danger">Passsword is required</p>';
+                            } ?>
+                        </div>
+                        <div class="button">
+                            <button class="create-btn" type="submit" name="createbtn">Create</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <style>
-    .main-div{
-        width: 100vw; height: 90vh; display: flex; flex-direction: row; margin-top: -2.45rem;
+    .main-div {
+        width: 100vw;
+        height: 90vh;
+        display: flex;
+        flex-direction: row;
+        margin-top: -2.45rem;
     }
-    .page-trainer-sidenav{
-        margin-top: -1.99rem; width: 20vw;
+
+    .page-trainer-sidenav {
+        margin-top: -1.99rem;
+        width: 20vw;
     }
-    .div-1{
-        height: 80vh; margin-left: 15rem;
+
+    .div-1 {
+        height: 80vh;
+        margin-left: 15rem;
     }
-    .container{
-        padding:3rem 0rem;
+
+    .card {
+        border-radius: 1rem;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        flex-direction: row;
+        gap: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40vw;
+        height: 75vh;
+        margin-top: 3rem
     }
-    .div-2{
-        flex-direction: row; display:flex; justify-content:center; align-items:center;
+
+    .card-body {
+        padding: 2rem;
+        width: 35vw;
+        color: black
     }
-    .div-3{
-        flex-direction:column; width: 40vw;
-    }
-    .card{
-        border-radius: 1rem; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    }
-    .div-4{
-        flex-direction:row; gap:0; display:flex; justify-content:center; align-items:center; width: 40vw;
-    }
-    .div-5{
-        display:flex; justify-content:center; align-items:center; height: 75vh; width: 40vw;
-    }
-    .card-body{
-        padding:2rem; color:black
-    }
-    form{
+
+    form {
         font-size: 16px
     }
-    form h2{
-        font-weight:bold; display:flex; justify-content:center; align-items:center;color: #315B87;margin-top:-2rem;
+
+    form h2 {
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #315B87;
+        margin-top: -2rem;
     }
-    .form-outline{
-        margin-bottom:1rem;
+
+    .form-outline {
+        margin-bottom: 1rem;
     }
-    .form-label{
+
+    .form-label {
         font-weight: 600;
     }
-    .button{
-        padding-top:1rem; margin-top: 1 rem; display:flex; justify-content:center; align-items:center;
+
+    .button {
+        padding-top: 1rem;
+        margin-top: 1 rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
+
     .create-btn {
         background-color: #315B87;
         color: #FAFAFA;
