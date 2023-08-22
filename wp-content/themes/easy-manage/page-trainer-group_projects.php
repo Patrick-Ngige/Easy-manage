8647b8d36aa97c120327675033d80759e5184561
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-22 23:50:00 +0300

    group projects: edited css

--- wp-content/themes/easy-manage/page-trainer-group_projects.php
+++ wp-content/themes/easy-manage/page-trainer-group_projects.php
@@ -112,12 +112,12 @@ if (isset($_POST['soft_delete'])) {
                                     <button type="submit" name="soft_delete" class="btn-soft-delete" data-bs-toggle="tooltip"
                                         data-bs-placement="top" title="Delete">
                                         <img src="http://localhost/easy-manage/wp-content/uploads/2023/06/pause-2.png"
-                                             alt="">
+                                             alt="delete button">
                                     </button>
                                     <a href="http://localhost/easy-manage/update-group-project/?id=<?php echo $project['group_id'] ?>"
                                          data-bs-toggle="tooltip" data-bs-placement="top" title="Edit" class="edit-btn"><img
                                             src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png"
-                                            style="width:25px;" alt=""></a> &nbsp;&nbsp;
+                                             alt="edit button"></a> &nbsp;&nbsp;
                                 </form>
                             </td>
                             </tr>
