bf4670de49b904fcec8c4085f819810ff6888c49
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-22 23:48:12 +0300

    group projects: edited css

--- wp-content/themes/easy-manage/page-trainer-group_projects.php
+++ wp-content/themes/easy-manage/page-trainer-group_projects.php
@@ -115,7 +115,7 @@ if (isset($_POST['soft_delete'])) {
                                              alt="">
                                     </button>
                                     <a href="http://localhost/easy-manage/update-group-project/?id=<?php echo $project['group_id'] ?>"
-                                        style="padding:6px" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit" class="edit-btn"><img
+                                         data-bs-toggle="tooltip" data-bs-placement="top" title="Edit" class="edit-btn"><img
                                             src="http://localhost/easy-manage/wp-content/uploads/2023/06/edit.png"
                                             style="width:25px;" alt=""></a> &nbsp;&nbsp;
                                 </form>
